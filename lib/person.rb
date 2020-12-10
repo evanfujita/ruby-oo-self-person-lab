f7a4f844296655351d1bc1bd581bc780b627f800
Marc Ferraro <marc.ferraro1@gmail.com>
2020-12-10 16:47:16 -0600

    version 2

--- lib/person.rb
+++ lib/person.rb
@@ -3,8 +3,8 @@ require 'pry'
 class Person
 
     @@all = []
-    attr_reader :name, :happiness
-    attr_writer :hygiene
+    attr_reader :name, :happiness, :hygiene
+    #attr_writer 
     attr_accessor :bank_account
 
 
@@ -20,17 +20,50 @@ class Person
         @@all
     end
 
-    def happiness
-        if @happiness
-
+    def happiness=(happiness)
+        @happiness = happiness
+        if @happiness > 10
+            @happiness = 10
+        elsif @happiness < 0
+            @happiness = 0
+        end
+        @happiness
     end
     
-    def work_out
-        self.happiness = -2
+    def hygiene=(hygiene)
+        @hygiene = hygiene
+        if @hygiene > 10
+            @hygiene = 10
+        elsif @hygiene < 0
+            @hygiene = 0
+        end
+        @hygiene
     end
+
+    def happy?
+        self.happiness > 7
+    end
+
+    def clean?
+        self.hygiene > 7
+    end
+
+    def work_out
+        self.happiness += 2
+        self.hygiene += -3
+        return "Another one bites the dust!"
+    end
+
+    def get_paid(salary)
+        self.bank_account += salary
+        return "all about the benjamins"
+    end
+
+
 
 end
 
 marc = Person.new("Marc")
 
 binding.pry
+1
