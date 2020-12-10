b3592ea39e0955ade30b0d7d0353199859e35dae
Evan <evanfujita@gmail.com>
2020-12-10 17:16:14 -0600

    Final Version

--- lib/person.rb
+++ lib/person.rb
@@ -1,12 +1,12 @@
 require 'pry'
 
 class Person
-
+    
     @@all = []
-    attr_reader :name, :happiness
-    attr_writer :hygiene
+    
+    attr_reader :name, :happiness, :hygiene
+    #attr_writer 
     attr_accessor :bank_account
-
 
     def initialize(name)
         @name = name
@@ -20,17 +20,70 @@ class Person
         @@all
     end
 
-    def happiness
-        if @happiness
-
+    def happiness=(happiness)
+        @happiness = happiness
+        if @happiness > 10
+            @happiness = 10
+        elsif @happiness < 0
+            @happiness = 0
+        end
+        @happiness
     end
-    
+
+    def hygiene=(hygiene)
+        @hygiene = hygiene
+        if @hygiene > 10
+            @hygiene = 10
+        elsif @hygiene < 0
+            @hygiene = 0
+        end
+        @hygiene
+    end
+
+    def happy?
+        self.happiness > 7
+    end
+
+    def clean?
+        self.hygiene > 7
+    end
+
     def work_out
-        self.happiness = -2
+        self.happiness += 2
+        self.hygiene += -3
+        return "♪ another one bites the dust ♫"
     end
 
+    def take_bath 
+        self.hygiene += 4
+        return "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def call_friend(friend)
+        self.happiness += 3
+        friend.happiness += 3
+        return "Hi #{friend.name}! It's #{@name}. How are you?"
+    end
+
+    def start_conversation(person ,topic )
+        if topic == "politics"
+            self.happiness -= 2
+            person.happiness -= 2
+            return "blah blah partisan blah lobbyist"
+        elsif topic == "weather"
+            self.happiness += 1
+            person.happiness += 1
+            return "blah blah sun blah rain"
+        else
+            return "blah blah blah blah blah"
+        end
+    end
+
+    def get_paid(salary)
+        self.bank_account += salary
+        return "all about the benjamins"
+    end
 end
 
 marc = Person.new("Marc")
-
-binding.pry
+#binding.pry
