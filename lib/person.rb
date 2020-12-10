f24b85e1249fe67520ff4b56abbbd6db64db9e12
Evan <evanfujita@gmail.com>
2020-12-10 15:11:27 -0600

    person class defined

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,36 @@
-# your code goes here
+require 'pry'
+
+class Person
+
+    @@all = []
+    attr_reader :name, :happiness
+    attr_writer :hygiene
+    attr_accessor :bank_account
+
+
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def happiness
+        if @happiness
+
+    end
+    
+    def work_out
+        self.happiness = -2
+    end
+
+end
+
+marc = Person.new("Marc")
+
+binding.pry
