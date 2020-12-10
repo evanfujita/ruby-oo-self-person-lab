519d2e848b34787c38505f559a2298ebddd42f70
Marc Ferraro <marc.ferraro1@gmail.com>
2020-12-10 17:31:52 -0600

    Final Version

--- lib/person.rb
+++ lib/person.rb
@@ -1,12 +1,10 @@
-require 'pry'
-
 class Person
 
     @@all = []
-    attr_reader :name, :happiness, :hygiene
-    #attr_writer 
-    attr_accessor :bank_account
 
+    attr_reader :name, :happiness, :hygiene
+    
+    attr_accessor :bank_account
 
     def initialize(name)
         @name = name
@@ -29,7 +27,7 @@ class Person
         end
         @happiness
     end
-    
+
     def hygiene=(hygiene)
         @hygiene = hygiene
         if @hygiene > 10
@@ -51,19 +49,36 @@ class Person
     def work_out
         self.happiness += 2
         self.hygiene += -3
-        return "Another one bites the dust!"
+        return "♪ another one bites the dust ♫"
+    end
+
+    def take_bath
+        self.hygiene += 4
+        return "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def call_friend(friend)
+        self.happiness += 3
+        friend.happiness += 3
+        return "Hi #{friend.name}! It's #{@name}. How are you?"
+    end
+
+    def start_conversation(person ,topic )
+        if topic == "politics"
+            self.happiness -= 2
+            person.happiness -= 2
+            return "blah blah partisan blah lobbyist"
+        elsif topic == "weather"
+            self.happiness += 1
+            person.happiness += 1
+            return "blah blah sun blah rain"
+        else
+            return "blah blah blah blah blah"
+        end
     end
 
     def get_paid(salary)
         self.bank_account += salary
         return "all about the benjamins"
     end
-
-
-
 end
-
-marc = Person.new("Marc")
-
-binding.pry
-1
